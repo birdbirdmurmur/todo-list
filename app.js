a1386581e4000efc51a0447b98f09c7c8c8ed089
BirdBird <birdbirdmurmur@gmail.com>
2023-07-25 21:03:50 +0800

    feat: add dotenv

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv/config'
 
+if (process.env.NODE_ENV !== 'production') {
+    dotenv
+}
 const app = express()
 const port = 3000
+
+mongoose.connect(process.env.MONGODB_URI)
+const db = mongoose.connection
+
+db.on('error', () => {
+    console.log('mongodb error!')
+})
+db.once('open', () => {
+    console.log('mongodb connected!')
+})
+
 
 app.get('/', (req, res) => {
     res.send('Hello World')
