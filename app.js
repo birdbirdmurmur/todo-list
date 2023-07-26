df84dec244ccc12e072bbab71dd58d82ecd20ef6
BirdBird <birdbirdmurmur@gmail.com>
2023-07-26 16:39:20 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -71,6 +71,14 @@ app.post('/todos/:id/edit', (req, res) => {
         .catch(error => console.log(error))
 })
 
+app.post('/todos/:id/delete', (req, res) => {
+    const id = req.params.id
+    return Todo.findById(id)
+        .then(todo => todo.deleteOne())
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+})
+
 app.listen(port, () => {
     console.log(`App is listening on http://localhost:${port}.`)
 })
