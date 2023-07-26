ee566f86bebdf66716fc31907a94d27fad6a2134
BirdBird <birdbirdmurmur@gmail.com>
2023-07-26 20:43:34 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import methodOverride from "method-override"
 
-import Todo from "./models/todo.js"; // Todo model
+// import Todo from "./models/todo.js"; // Todo model
+import routes from "./routes/index.js"
 import 'dotenv/config'
 
 if (process.env.NODE_ENV !== 'production') { }
@@ -26,63 +27,7 @@ app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
-// Controller
-app.get('/', (req, res) => {
-    Todo.find()
-        .lean()
-        .sort({ _id: 'asc' })
-        .then(todos => res.render('index', { todos }))
-        .catch(error => console.error(error))
-})
-
-app.post('/todos', (req, res) => {
-    const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
-    return Todo.create({ name })     // 存入資料庫
-        .then(() => res.redirect('/')) // 新增完成後導回首頁
-        .catch(error => console.log(error))
-})
-
-app.get('/todos/new', (req, res) => {
-    return res.render('new')
-})
-
-app.get('/todos/:id', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-        .lean()
-        .then((todo) => res.render('detail', { todo }))
-        .catch(error => console.log(error))
-})
-
-app.get('/todos/:id/edit', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-        .lean()
-        .then((todo) => res.render('edit', { todo }))
-        .catch(error => console.log(error))
-})
-
-app.put('/todos/:id', (req, res) => {
-    const id = req.params.id
-    const { name, isDone } = req.body
-    return Todo.findById(id)
-        .then(todo => {
-            todo.name = name
-            todo.isDone = isDone === 'on'
-            return todo.save()
-        })
-        .then(() => res.redirect(`/todos/${id}`))
-        .catch(error => console.log(error))
-})
-
-app.delete('/todos/:id', (req, res) => {
-    const id = req.params.id
-    return Todo.findById(id)
-        .then(todo => todo.deleteOne())
-        .then(() => res.redirect('/'))
-        .catch(error => console.log(error))
-})
+app.use(routes)
 
 app.listen(port, () => {
     console.log(`App is listening on http://localhost:${port}.`)
---
+++ routes/modules/home.js
@@ -0,0 +1,13 @@
+import express from "express"
+import Todo from "../../models/todo.js"
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Todo.find()
+        .lean()
+        .sort({ _id: 'asc' }) // desc
+        .then(todos => res.render('index', { todos }))
+        .catch(error => console.error(error))
+})
+
+export default router
