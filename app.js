4c40b38bc642b1d2daaad36ee6a183739db8eea7
BirdBird <birdbirdmurmur@gmail.com>
2023-07-26 15:08:43 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { engine } from "express-handlebars";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import Todo from "./models/todo.js"; // Todo model
 import 'dotenv/config'
-import Todo from "./models/todo.js";
 
 if (process.env.NODE_ENV !== 'production') { }
 
@@ -21,12 +22,25 @@ db.once('open', () => {
 
 app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
+// Controller
 app.get('/', (req, res) => {
     Todo.find()
         .lean()
         .then(todos => res.render('index', { todos }))
         .catch(error => console.error(error))
+})
+
+app.post('/todos', (req, res) => {
+    const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+    return Todo.create({ name })     // 存入資料庫
+        .then(() => res.redirect('/')) // 新增完成後導回首頁
+        .catch(error => console.log(error))
+})
+
+app.get('/todos/new', (req, res) => {
+    return res.render('new')
 })
 
 app.listen(port, () => {
